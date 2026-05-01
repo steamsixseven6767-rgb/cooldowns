b06573ac41e7fd2243c25ce4ac515645f67d65fb
steamsixseven6767-rgb <steamsixseven6767@gmail.com>
2026-05-01 10:38:23 +0300

    Delete src/main/java/com/cooldownhud/src/main/java/com/cooldownhud/client directory

--- src/main/java/com/cooldownhud/src/main/java/com/cooldownhud/client/ICooldownManagerAccess.java
+++
@@ -1,9 +0,0 @@
-package com.cooldownhud.client;
-
-/**
- * Interface injected into ItemCooldownManager via Mixin
- * to expose raw tick data.
- */
-public interface ICooldownManagerAccess {
-    int cooldownhud$getTicksRemaining(net.minecraft.item.Item item);
-}
